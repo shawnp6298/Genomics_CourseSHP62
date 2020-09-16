0301bf746be90af7b19e5511517884f3d01b58ca
Shawn Portwood <sportwood@umass.edu>
2020-09-16 07:24:17 -0400

    Lab 1

---
+++ EvoGenoLab1.R
@@ -0,0 +1,25 @@
+##Exercise 1
+
+x <- 3
+y <- 15
+x+y
+x-y
+x*y
+x/y
+
+##Exercise 2
+
+x <- 2
+y <- 3
+z = x^5>y^4
+
+##Exercise 3
+
+x <- c(22,62,148,43,129)
+sum(x)
+x/sum(x)
+
+##Exercise 4
+
+nucleotides <- c("A","T","C","G")
+sort(nucleotides)
